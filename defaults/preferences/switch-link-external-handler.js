6df1549f349c67ce0ed8503cba3d57d60c8f7439
Hiroshi Hatake <hatake@clear-code.com>
2015-01-07 17:27:05 +0900

    Add priority property setting sample

--- defaults/preferences/switch-link-external-handler.js
+++ defaults/preferences/switch-link-external-handler.js
@@ -2,10 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Sample
+// Domain Sample
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.0", "http://*.microsoft.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.1", "https://*.microsoft.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.2", "https://*.mozilla.org");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.0", "https://*.clear-code.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.1", "https://github.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.2", "http://?.hatena.ne.jp");
+// Priority Sample
+// pref("extensions.switch-link-external-handler@clear-code.com.chrome.priority", 50);
+// pref("extensions.switch-link-external-handler@clear-code.com.ie.priority", 20);
