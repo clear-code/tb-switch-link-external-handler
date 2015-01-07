702d717dd9b1074b71096dcfc062d8ca8ee0ba5b
Hiroshi Hatake <hatake@clear-code.com>
2015-01-07 17:31:32 +0900

    Add pattern conflict sample
    This pattern setting expects that 'groonga.org' is opened by Google Chrome.

--- defaults/preferences/switch-link-external-handler.js
+++ defaults/preferences/switch-link-external-handler.js
@@ -6,9 +6,11 @@
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.0", "http://*.microsoft.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.1", "https://*.microsoft.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.2", "https://*.mozilla.org");
+// pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.3", "http://groonga.org");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.0", "https://*.clear-code.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.1", "https://github.com");
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.2", "http://?.hatena.ne.jp");
-// Priority Sample
+// pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.3", "http://groonga.org");
+// // Priority Sample
 // pref("extensions.switch-link-external-handler@clear-code.com.chrome.priority", 50);
 // pref("extensions.switch-link-external-handler@clear-code.com.ie.priority", 20);
