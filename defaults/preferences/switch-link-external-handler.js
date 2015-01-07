4bb20838076e7bd6bce2db8295eb4e03fcc3e236
Hiroshi Hatake <hatake@clear-code.com>
2015-01-07 13:05:03 +0900

    Add sample setting

---
+++ defaults/preferences/switch-link-external-handler.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Sample
+// pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.0", "http://*.microsoft.com");
+// pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.1", "https://*.microsoft.com");
+// pref("extensions.switch-link-external-handler@clear-code.com.ie.patterns.2", "https://*.mozilla.org");
+// pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.0", "https://*.clear-code.com");
+// pref("extensions.switch-link-external-handler@clear-code.com.chrome.patterns.1", "https://github.com");
